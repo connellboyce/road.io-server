548e5f6378efee591b2bf13ce00334b2c0e90d79
Connell Boyce <connell.p.boyce@gmail.com>
2021-02-23 17:39:57 -0500

    Made intial Map Component and converted App to one
    - App.js is a component now, not function
    - Created Map.js to be the map component eventually

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,50 @@
+import React, {Component} from 'react';
 import logo from './globe-png-9acqaaMTM.png';
 import './App.css';
+import Map from './Map.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Click here to enter
-        </p>
-        <a href="./App.css" className="button1">Button</a>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: null,
+            isLoaded: false
+        };
+    }
+
+    componentDidMount() {
+        this.setState({
+            isLoaded: true
+        });
+    }
+
+    render() {
+        const { error, isLoaded } = this.state;
+
+            return (
+                <div className="App">
+                    <header className="App-header">
+                        <img src={logo} className="App-logo" alt="logo"/>
+                        <p>
+                            Click here to enter
+                        </p>
+                        <a href="./App.css" className="button1">Button</a>
+                        <a
+                            className="App-link"
+                            href="https://reactjs.org"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            Learn React!
+                        </a>
+                        <Map />
+                    </header>
+                </div>
+            );
+
+    }
+
+
 }
 
 export default App;
