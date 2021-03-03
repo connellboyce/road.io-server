f46719ec7428471243150f9e9de8b161a7e6073e
Connell Boyce <connell.p.boyce@gmail.com>
2021-03-03 14:34:11 -0500

    Added field injection for API Keys
    - Created a secret controller
    - added a batch script for adding future environment variables
    - Allows to share keys between client and server

---
+++ src/main/java/com/group4/capstone/roadio/controller/SecretController.java
@@ -0,0 +1,20 @@
+package com.group4.capstone.roadio.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/secrets")
+public class SecretController {
+
+    @Value("${app.hereapi.key}")
+    private String hereAPIKey;
+
+    @GetMapping("/here")
+    public String getHereKey() {
+        return hereAPIKey;
+    }
+
+}
