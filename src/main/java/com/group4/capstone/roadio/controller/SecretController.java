d89dba01c3c7713f96f46fe311ce956657130e33
Connell Boyce <connell.p.boyce@gmail.com>
2021-03-03 19:53:31 -0500

    Allowed for cross origin requests
    - Previous commit did not include these files
      - Should have been included in last commit

--- src/main/java/com/group4/capstone/roadio/controller/SecretController.java
+++ src/main/java/com/group4/capstone/roadio/controller/SecretController.java
@@ -1,12 +1,14 @@
 package com.group4.capstone.roadio.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/secrets")
+@CrossOrigin
 public class SecretController {
 
     @Value("${app.hereapi.key}")
