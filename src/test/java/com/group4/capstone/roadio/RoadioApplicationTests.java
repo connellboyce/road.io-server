0f49d3441c9c6ed956783b75cc56ee9df1ccf40e
Connell Boyce <connell.p.boyce@gmail.com>
2021-04-12 17:40:59 -0400

    Created initial Mockito tests for controller

--- src/test/java/com/group4/capstone/roadio/RoadioApplicationTests.java
+++ src/test/java/com/group4/capstone/roadio/RoadioApplicationTests.java
@@ -1,13 +1,21 @@
 package com.group4.capstone.roadio;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class RoadioApplicationTests {
 
+	@Autowired
+	private WebApplicationContext context;
+
+
 	@Test
 	void contextLoads() {
+		assertThat(context).isNotNull();
 	}
-
 }
