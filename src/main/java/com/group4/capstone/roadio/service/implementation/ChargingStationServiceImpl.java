14dae904c9367c50530b40a769b5c6e680381ccc
Connell Boyce <connell.p.boyce@gmail.com>
2021-04-12 21:54:55 -0400

    Added Mockito mocks to test Station Services
    - tests upstream URL creation for routing
    - tests upstream URL creation for nearby stations

--- src/main/java/com/group4/capstone/roadio/service/implementation/ChargingStationServiceImpl.java
+++ src/main/java/com/group4/capstone/roadio/service/implementation/ChargingStationServiceImpl.java
@@ -1,6 +1,8 @@
 package com.group4.capstone.roadio.service.implementation;
 
 import com.group4.capstone.roadio.service.ChargingStationService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -9,12 +11,13 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Service
 public class ChargingStationServiceImpl implements ChargingStationService {
 
+    private final Logger logger = LoggerFactory.getLogger(ChargingStationServiceImpl.class);
     private final WebClient webClient;
 
-    @Value("${app.hereapi.key}")
+    @Value("${app.hereapi.key}:testMe")
     String hereKey;
 
-    @Value("${app.nrel.key}")
+    @Value("${app.nrel.key}:testMe")
     private String nrelKey;
 
     public ChargingStationServiceImpl(WebClient.Builder webClientBuilder) {
@@ -25,6 +28,7 @@ public class ChargingStationServiceImpl implements ChargingStationService {
     @Override
     public String getStationsAlongRoute(String origin, String outletType, String destination, String freeFlowSpeedTable, String initialCharge, String maxCharge, String chargingCurve, String maxChargeAfterChargingStation) {
         String url = "https://router.hereapi.com/v8/routes?departureTime=any&origin="+origin+"&ev[connectorTypes]="+outletType+"&transportMode=car&destination="+destination+"&return=polyline&ev[freeFlowSpeedTable]="+freeFlowSpeedTable+"&ev[makeReachable]=true&ev[initialCharge]="+initialCharge+"&ev[maxCharge]="+maxCharge+"&ev[chargingCurve]="+chargingCurve+"&ev[maxChargeAfterChargingStation]="+maxChargeAfterChargingStation+"&apiKey="+ hereKey;
+        logger.debug("getStationsAlongRoute URL={}",url);
 
         return webClient.get()
                 .uri(url)
@@ -36,6 +40,8 @@ public class ChargingStationServiceImpl implements ChargingStationService {
     @Override
     public String getStationsNearMe(String latitude, String longitude, String radius) {
         String url = "https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?latitude="+latitude+"&longitude="+longitude+"&radius="+radius+"&api_key="+nrelKey+"&fuel_type=ELEC";
+        logger.debug("getStationsNearMe URL={}",url);
+
         return webClient.get()
                 .uri(url)
                 .retrieve()
--- src/test/java/com/group4/capstone/roadio/service/implementation/ChargingStationServiceImplTest.java
+++ src/test/java/com/group4/capstone/roadio/service/implementation/ChargingStationServiceImplTest.java
@@ -1,26 +1,99 @@
 package com.group4.capstone.roadio.service.implementation;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import com.group4.capstone.roadio.service.ChargingStationService;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.times;
 
 class ChargingStationServiceImplTest {
 
-    @BeforeEach
-    void setUp() {
-    }
+    private ChargingStationService chargingStationService;
+    private WebClient.Builder webClientBuilder;
+    private WebClient webClient;
 
-    @AfterEach
-    void tearDown() {
+    @Test
+    void whenRoutingParametersArePresent_upstreamUrlIsProperlyBuilt() {
+        String expectedURI = "https://router.hereapi.com/v8/routes?departureTime=any&origin=40.79358,-77.86056&ev[connectorTypes]=iec62196Type1Combo&transportMode=car&destination=41.88425,-87.63245&return=polyline&ev[freeFlowSpeedTable]=110,0.165&ev[makeReachable]=true&ev[initialCharge]=45&ev[maxCharge]=60&ev[chargingCurve]=0,50,9,52,12,54,15,54,18,54,21,54,24,55,27,55,30,55,33,37,36,37,39,37,42,23,45,23,48,23,51,16,54,16,57,10,60,4&ev[maxChargeAfterChargingStation]=60&apiKey=null";
+
+        String origin="40.79358,-77.86056";
+        String chargerType="iec62196Type1Combo";
+        String destination="41.88425,-87.63245";
+        String speedTable="110,0.165";
+        String currentCharge="45";
+        String maxCharge="60";
+        String chargingCurve="0,50,9,52,12,54,15,54,18,54,21,54,24,55,27,55,30,55,33,37,36,37,39,37,42,23,45,23,48,23,51,16,54,16,57,10,60,4";
+        String maxChargeAfterStation="60";
+        String testResult = "{\"routes\": []}";
+
+        webClientBuilder = mock(WebClient.Builder.class);
+        webClient = mock(WebClient.class);
+
+        WebClient.RequestHeadersUriSpec<?> webClientGet = mock(WebClient.RequestHeadersUriSpec.class);
+        WebClient.RequestHeadersSpec<?> webClientUri = mock(WebClient.RequestHeadersSpec.class);
+        WebClient.ResponseSpec webClientRetrieve = mock(WebClient.ResponseSpec.class);
+        Mono<String> stringMono = mock(Mono.class);
+
+        doReturn(webClient).when(webClientBuilder).build();
+
+        doReturn(webClientGet).when(webClient).get();
+        doReturn(webClientUri).when(webClientGet).uri(expectedURI);
+        doReturn(webClientRetrieve).when(webClientUri).retrieve();
+        doReturn(stringMono).when(webClientRetrieve).bodyToMono(String.class);
+        doReturn(testResult).when(stringMono).block();
+
+        chargingStationService = new ChargingStationServiceImpl(webClientBuilder);
+        verify(webClientBuilder, times(1)).build();
+
+        String stationsAlongRoute = chargingStationService.getStationsAlongRoute(origin, chargerType, destination, speedTable, currentCharge, maxCharge, chargingCurve, maxChargeAfterStation);
+        verify(webClient, times(1)).get();
+        verify(webClientGet, times(1)).uri(expectedURI);
+        verify(webClientUri, times(1)).retrieve();
+        verify(webClientRetrieve, times(1)).bodyToMono(String.class);
+        verify(stringMono, times(1)).block();
+
+        Assert.assertNotNull(stationsAlongRoute);
+        Assert.assertEquals(testResult, stationsAlongRoute);
     }
 
     @Test
-    void getStationsAlongRoute() {
-    }
+    void whenNearbyStationParametersArePresent_upstreamUrlIsProperlyBuilt() {
+        String expectedURI = "https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?latitude=40.79358&longitude=-77.86056&radius=25&api_key=null&fuel_type=ELEC";
 
-    @Test
-    void getStationsNearMe() {
+        String latitude = "40.79358";
+        String longitude = "-77.86056";
+        String radius = "25";
+        String testResult = "{\"stations\": []}";
+
+        webClientBuilder = mock(WebClient.Builder.class);
+        webClient = mock(WebClient.class);
+
+        WebClient.RequestHeadersUriSpec<?> webClientGet = mock(WebClient.RequestHeadersUriSpec.class);
+        WebClient.RequestHeadersSpec<?> webClientUri = mock(WebClient.RequestHeadersSpec.class);
+        WebClient.ResponseSpec webClientRetrieve = mock(WebClient.ResponseSpec.class);
+        Mono<String> stringMono = mock(Mono.class);
+
+        doReturn(webClient).when(webClientBuilder).build();
+
+        doReturn(webClientGet).when(webClient).get();
+        doReturn(webClientUri).when(webClientGet).uri(expectedURI);
+        doReturn(webClientRetrieve).when(webClientUri).retrieve();
+        doReturn(stringMono).when(webClientRetrieve).bodyToMono(String.class);
+        doReturn(testResult).when(stringMono).block();
+
+        chargingStationService = new ChargingStationServiceImpl(webClientBuilder);
+        verify(webClientBuilder, times(1)).build();
+
+        String nearbyStations = chargingStationService.getStationsNearMe(latitude, longitude, radius);
+        verify(webClient, times(1)).get();
+        verify(webClientGet, times(1)).uri(expectedURI);
+        verify(webClientUri, times(1)).retrieve();
+        verify(webClientRetrieve, times(1)).bodyToMono(String.class);
+        verify(stringMono, times(1)).block();
+
+        Assert.assertNotNull(nearbyStations);
+        Assert.assertEquals(testResult, nearbyStations);
     }
 }
